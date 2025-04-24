65f7e5605a9a1571c8a78cdcbd45cead21a0998c
NenadN22 <tonbe22@gmail.com>
2025-04-24 21:28:29 +0200

    Initial input logic: handle number and operator clicks, store first and second numbers, basic display update

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 }
+let currentOperator;
+let currentNumber = "";
+let operator;
+let display;
+display = document.querySelector('.display')
+console.log(typeof(operator))
+let firstNumber = "";
+let secondNumber = "";
+currentOperator = document.querySelectorAll('.operator')
+currentOperator.forEach(clickedOperator => {
+    clickedOperator.addEventListener('click', () => {
+        operator = clickedOperator.textContent;
+        console.log(operator)
+        console.log(typeof(operator))
+    })
+})
+currentNumber = document.querySelectorAll('.number')
+currentNumber.forEach(clickedNumber => {
+    clickedNumber.addEventListener('click', () => {
+        currentNumber = clickedNumber.textContent
+        console.log(currentNumber)
+        if(!operator) {
+            firstNumber += currentNumber
+            console.log(firstNumber)
+        }  else {
+            currentNumber = ""
+            currentNumber = clickedNumber.textContent
+            secondNumber += currentNumber;
+            console.log(secondNumber)
+        }
+        populateDisplay()
+    })
+   
+    
+})
+function populateDisplay() {
+    display.innerHTML = firstNumber
+    
+}
+
+function operate (firstNumber, operator, secondNumber) {
+
+}
