b97dbf6e2ce4e551ea216942b1c31aa1e7fd0259
NenadN22 <tonbe22@gmail.com>
2025-04-24 20:35:16 +0200

    Need to change things i make it too complicated

--- script.js
+++ script.js
@@ -11,28 +11,3 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 }
-
-let number = "";
-let operator;
-let anotherNumber;
-
-let display = document.querySelector('.display')
-
-let numbers = document.querySelectorAll('.number')
-numbers.forEach(clickedNumber => 
-    clickedNumber.addEventListener('click', ()=> {
-        number += clickedNumber.textContent
-        console.log(number)
-        populateDisplay()
-    })
-    
-)
-function populateDisplay () {
-    display.innerHTML = number
-    console.log(display)
-  
-}
-
-function Operate(operator,number,anotherNumber) {
-
-}
