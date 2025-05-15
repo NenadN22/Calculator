fb9ed05bc32c3e43da1a7b3bc699711fbd89544f
NenadN22 <tonbe22@gmail.com>
2025-05-15 21:20:39 +0200

    feat: implemented number and operator click handlers, display update logic, and variable setup for calculator input

--- script.js
+++ script.js
@@ -12,6 +12,40 @@ function divide (a,b) {
     return a / b;
 }
 
-let firstNumber;
+let firstGlobalNumber = "";
 let globalOperator;
-let secondNumber;
+let secondGlobalNumber = "";
+let operator = "";
+let display = document.querySelector('.display');
+let allNumbers = document.querySelectorAll('.number');
+let allOperators = document.querySelectorAll(".operator");
+allOperators.forEach(operators => {
+    operators.addEventListener('click',() => {
+        operator = operators.textContent;
+        console.log(operator);
+        populateDisplay();
+    })
+})
+allNumbers.forEach(numbers => {
+    numbers.addEventListener('click', () => {
+        console.log(numbers)
+        if(!operator) {
+            firstGlobalNumber += numbers.textContent
+        } else {
+            secondGlobalNumber += numbers.textContent
+        }
+        populateDisplay()
+    })
+})
+
+console.log(firstGlobalNumber)
+console.log(secondGlobalNumber)
+function operate(firstNumber,secondNumber) {
+
+}
+function populateDisplay(){
+    display.textContent = firstGlobalNumber
+    display.textContent += operator
+    display.textContent += secondGlobalNumber
+
+}
