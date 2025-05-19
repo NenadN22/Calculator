69c2790ce3c325e48657b630c4a81fef86884b6d
NenadN22 <tonbe22@gmail.com>
2025-05-19 23:11:50 +0200

    Add delete button functionality for removing last input digit
    Implemented logic to handle backspace for both first and second numbers,
    depending on whether an operator has been selected.
    Updated display accordingly after deletion.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 }
-
+let del  = document.querySelector('.delete')
 let firstGlobalNumber = "";
 let globalOperator;
 let secondGlobalNumber = "";
@@ -26,18 +26,27 @@ allOperators.forEach(operators => {
         populateDisplay();
     })
 })
+  del.addEventListener('click', () => {
+    if(!operator) {
+        firstGlobalNumber = firstGlobalNumber.slice(0,firstGlobalNumber.length - 1)
+        populateDisplay()
+    } else if (firstGlobalNumber != "") {
+        secondGlobalNumber = secondGlobalNumber.slice(0,secondGlobalNumber.length - 1)
+        populateDisplay()
+    }
+})
 allNumbers.forEach(numbers => {
     numbers.addEventListener('click', () => {
         console.log(numbers)
         if(!operator) {
-            firstGlobalNumber += numbers.textContent
+            firstGlobalNumber += numbers.textContent 
         } else {
             secondGlobalNumber += numbers.textContent
         }
         populateDisplay()
     })
 })
-let result;
+let result = "";
 let equal = document.querySelector(".equal")
 equal.addEventListener('click', () => {
     operate(firstGlobalNumber,secondGlobalNumber)
@@ -45,18 +54,42 @@ equal.addEventListener('click', () => {
 console.log(firstGlobalNumber)
 console.log(secondGlobalNumber)
 function operate(firstNumber,secondNumber) {
-    firstNumber = parseInt(firstGlobalNumber)
-    secondNumber = parseInt(secondGlobalNumber)
-    if(operator == "+") {
+   
+    firstNumber = parseFloat(firstGlobalNumber)
+    secondNumber = parseFloat(secondGlobalNumber)
+  
+    
+    if (operator == "÷"  &&   firstNumber == 0 || secondNumber == 0) {
+        result = 'Cant Divide with 0!';
+        display.textContent = result;
+        firstGlobalNumber = "";
+        secondGlobalNumber = "";
+        operator = "";
+        return
+    }
+    else if (operator == "+") {
         result = add(firstNumber,secondNumber)
+
+        
     } else if (operator == "-") {
         result = subtract(firstNumber,secondNumber)
+        
     } else if (operator == "x") {
         result = multiply(firstNumber,secondNumber)
+        
     } else if ( operator == "÷") {
         result = divide(firstNumber,secondNumber)
+        
+    } 
+    if (typeof(result) === "number") {
+        result = Math.round(result * 1000) / 1000
+        result = parseFloat(result.toFixed(2))
     }
-    firstGlobalNumber = result;
+    
+   
+    firstGlobalNumber = result
+   
+
     console.log(typeof(firstGlobalNumber))
     operator = ""
     secondGlobalNumber = ""
